81429086ce0fabd47a74d5d2159507bbaa6fdba2
J Mulder <jlmulder2014@gmail.com>
2021-01-17 09:33:02 -0800

    update .json log

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -38,7 +38,7 @@ const SearchBooks = () => {
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
-
+      console.log(response);
       const { items } = await response.json();
 
       const bookData = items.map((book) => ({
